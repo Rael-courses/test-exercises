f0b7152e50e37dbd683ce6fce33b1a93e25a1c37
Rael Calitro <rael06@hotmail.fr>
2024-04-17 23:41:31 +0200

    wip

--- src/exercises/addStringOfNumbers/index.test.ts
+++ src/exercises/addStringOfNumbers/index.test.ts
@@ -45,8 +45,10 @@ describe("addStringOfNumbers", () => {
   it.each([
     { input: "2,3", expected: 5 },
     { input: "-2.5,4", expected: 1.5 },
+    { input: "1,2,5,9", expected: 17 },
+    { input: "1,-3.2,3.7,2", expected: 3.5 },
   ])(
-    "should add two comma separated numbers in a string",
+    "should add any quantity of comma separated numbers in a string",
     ({ input, expected }) => {
       // arrange
       // act
@@ -57,7 +59,7 @@ describe("addStringOfNumbers", () => {
     }
   );
 
-  it("should throw an error when given a string with wrong separator", () => {
+  it("should throw when given a string with wrong separator", () => {
     // arrange
     const input = "-2.5;3";
 
@@ -67,4 +69,47 @@ describe("addStringOfNumbers", () => {
     // assert
     expect(act).toThrow("Invalid characters");
   });
+
+  it.each([
+    { input: "//;\n1;2", expected: 3 },
+    { input: "//:\n-2.5:4", expected: 1.5 },
+    { input: "//;\n1;2;5;9", expected: 17 },
+  ])(
+    "should add any quantity of numbers separated by a custom character in a string",
+    ({ input, expected }) => {
+      // arrange
+      // act
+      const result = addStringOfNumbers(input);
+
+      // assert
+      expect(result).toBe(expected);
+    }
+  );
+
+  it.each(["//.\n1.2", "//-\n-2.5-4"])(
+    "should throw when the custom separator is not valid",
+    (input) => {
+      // arrange
+      // act
+      const act = () => addStringOfNumbers(input);
+
+      // assert
+      expect(act).toThrow("Invalid custom separator");
+    }
+  );
+
+  it.each([
+    { input: "//;;;\n1;;;2", expected: 3 },
+    { input: "//::\n-2.5::4", expected: 1.5 },
+  ])(
+    "should add any quantity of numbers separated by a custom character in a string",
+    ({ input, expected }) => {
+      // arrange
+      // act
+      const result = addStringOfNumbers(input);
+
+      // assert
+      expect(result).toBe(expected);
+    }
+  );
 });
--- src/exercises/addStringOfNumbers/index.ts
+++ src/exercises/addStringOfNumbers/index.ts
@@ -1,17 +1,37 @@
+const defaultSeparator = ",";
+
 export function addStringOfNumbers(input: string) {
   if (input === "") return 0;
 
-  // Regex to catch any character that is not a digit, comma, dot or minus sign
-  const isContainingBadCharacters = /[^-,.\d]/.test(input);
+  const customSeparatorMatches = input.match(/^\/\/(.+)\n/);
+  const customSeparator = customSeparatorMatches
+    ? customSeparatorMatches[1]
+    : null;
+
+  const separator = customSeparator || defaultSeparator;
+  const isContainingBadSeparator = /[-.]/.test(separator);
+  if (isContainingBadSeparator) throw new Error("Invalid custom separator");
+
+  const isSeparatorMadeWithSameCharacters = !separator
+    .split("")
+    .some((char) => char !== separator[0]);
+  if (!isSeparatorMadeWithSameCharacters)
+    throw new Error("Invalid custom separator");
+
+  // Regex to catch any character that is not a digit, dot, minus sign or separator
+  const badCharactersRegex = new RegExp(`[^-.\\d${separator}]`);
+
+  const focusedExpression = customSeparator ? input.split("\n")[1] : input;
+  const isContainingBadCharacters = badCharactersRegex.test(focusedExpression);
   if (isContainingBadCharacters) throw new Error("Invalid characters");
 
-  if (!input.includes(",")) {
-    const num = Number(input);
+  if (!focusedExpression.includes(separator)) {
+    const num = Number(focusedExpression);
     if (isNaN(num)) throw new Error("Not valid number");
     return num;
   }
 
-  const arr = input.split(",");
+  const arr = focusedExpression.split(separator);
   return arr.reduce((acc, str) => {
     const num = Number(str);
     if (isNaN(num)) throw new Error("Not valid numbers");
